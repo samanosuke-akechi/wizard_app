ed905ac72f01b6032860ee43d73141c9107837b9
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-06-01 15:26:39 +0900

    addressesテーブル作成
    postal_code, address, user_idカラムを記述。
    コマンドでテーブル生成。

--- db/migrate/20210601062243_create_addresses.rb
+++ db/migrate/20210601062243_create_addresses.rb
@@ -1,7 +1,9 @@
 class CreateAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :addresses do |t|
-
+      t.integer :postal_code
+      t.text :address
+      t.references :user
       t.timestamps
     end
   end
